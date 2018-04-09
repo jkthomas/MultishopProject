3b253dc35d0ef683f54201ab300e22fb13ebf432
jkthomas <tom.januszek@gmail.com>
2018-04-09 21:37:19 +0200

    [PTRN] Added repository interface and product repository service created according to repository pattern.

---
+++ Multishop.Data/DAL/Services/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multishop.Data.DAL.Services.Repository
+{
+    interface IRepository<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetEntities();
+        T GetDetails(int entityId);
+        void Insert(T entity);
+        void Delete(int entityId);
+        void Update(T entity);
+        void Save();
+    }
+}
---
+++ Multishop.Data/DAL/Services/Repository/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using Multishop.Data.DAL.Context;
+using Multishop.Entities.ShopEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multishop.Data.DAL.Services.Repository
+{
+    class ProductRepository : IRepository<Product>
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public void Delete(int productId)
+        {
+            Product product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+        }
+
+        public Product GetDetails(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetEntities()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void Insert(Product product)
+        {
+            _dbContext.Products.Add(product);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _dbContext.Entry(product).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
